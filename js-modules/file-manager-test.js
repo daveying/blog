eda0b9ea968999c8491fec2d3cbd7bc9b1c38061
daveying <daveying@163.com>
2017-07-11 21:32:36 +0800

    can read the folder structure and can generate a html ordered list

--- js-modules/file-manager-test.js
+++ js-modules/file-manager-test.js
@@ -1,3 +1,8 @@
 var fm = require('./file-manager.js');
+var fs = require('fs');
 
 fm.updateStructure();
+fs.writeFile('result.html', fm.mainContent, 'utf8', () => {});
+//console.dir(fm.mainContent);
+
+
--- js-modules/file-manager.js
+++ js-modules/file-manager.js
@@ -1,20 +1,58 @@
 const fse = require('fs-extra');
 const fs = require('fs');
 const klaw = require('klaw');
+const path = require('path');
 
 
-var FileManager = function () {};
+var FileManager = function () {
+    this.structure = undefined;
+    this.mainContent = '';
+};
+const baseDir = path.resolve(__dirname, '../md-src');
+
+function formHtmlNode(type, id, calss, content) {
+    return `<${type} id='${id}' class='${calss}'>${content}</${type}>`;
+}
+
+function formList(dirObj, ordered) {
+    let type = 'ul';
+    if (ordered === undefined) type = 'ol';
+
+    var htmlStr = `<${type}>`;
+    let lenDir = dirObj.dirs.length;
+    for (let i = 0; i < lenDir; i++) {
+        htmlStr += formHtmlNode('li', dirObj.dirs[i].path, '', formList(dirObj.dirs[i], ordered));
+    }
+    let lenFiles = dirObj.files.length;
+    for (let i = 0; i < lenFiles; i++) {
+        htmlStr += formHtmlNode('li', dirObj.files[i].path, '', dirObj.files[i].path);
+    }
+    htmlStr += `</${type}>`;
+    return htmlStr;
+}
+
+function walkThrough(path) {
+    var files = fs.readdirSync(path, 'utf8');
+    var lenFiles = files.length;
+    var tree = {
+        dirs: [],
+        files: []
+    };
+    for (let i = 0; i < lenFiles; i++) {
+        let stat = fs.statSync(path + '/' + files[i]); // FIXME
+        if (stat.isDirectory()) {
+            tree.dirs.push(walkThrough(path + '/' + files[i]));
+        } else {
+            tree.files.push({path: path + '/' + files[i], fsstat: stat});
+        }
+    }
+    return tree;
+}
 
 FileManager.prototype.updateStructure = function () {
-    console.log('updateStructure');
-    var structure = [];
-    klaw('../md-src')
-        .on('data', function (item) {
-            structure.push(item.path);
-        })
-        .on('end', function () {
-            console.dir(structure);
-        });
+    this.structure = walkThrough(baseDir);
+    this.mainContent = formList(this.structure);
+    //console.dir(structure);
 }
 
 module.exports = new FileManager();
