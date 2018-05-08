f21cf0875a310049eb43b63cb9f7302fd130b15b
daveying <daveying@163.com>
2018-05-08 23:28:28 +0800

    add artical abstract partialy working

--- blog-front/src/components/AllArticals.vue
+++ blog-front/src/components/AllArticals.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>All articals page</div>
+  <v-container fluid mx-0 px-0>
+    <v-layout v-for="blog in blogs" :key="blog.id">
+      <v-flex xs12>
+        <ArticalAbstract
+          :title="blog.title"
+          :createdDate="blog.createdDate"
+          :viewCount="blog.viewCount"
+          :tags="blog.tags"
+        ></ArticalAbstract>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+import ArticalAbstract from '@/components/ArticalAbstract.vue'
+export default {
+  computed: {
+    blogs () {
+      return this.$store.getters.blogs
+    }
+  },
+  components: {ArticalAbstract}
+}
+</script>
--- blog-front/src/store/index.js
+++ blog-front/src/store/index.js
@@ -24,14 +24,20 @@ export const store = new Vuex.Store({
       let blogs = [
         {
           id: 'blog01',
+          title: '第一篇博客',
           createdDate: '2018-05-01',
+          viewCount: 1231,
+          tags: ['HTML', 'THREE'],
           imageUrl: '',
           abstract: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et fugiat blanditiis quo quam. Dolores possimus, mollitia minus, impedit quasi provident commodi alias ab repellat assumenda quia harum placeat nisi corrupti.',
           md: '# 在Ubuntu为特定用户安装全局NodeJS包\n\n在Ubuntu上通过NPM以全局方式安装NodeJS包时经常出现与路径访问权限的问题'
         },
         {
           id: 'blog02',
+          title: '第二篇博客',
           createdDate: '2018-05-02',
+          viewCount: 1232,
+          tags: ['HTML', 'WebGL'],
           imageUrl: '',
           abstract: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et fugiat blanditiis quo quam. Dolores possimus, mollitia minus, impedit quasi provident commodi alias ab repellat assumenda quia harum placeat nisi corrupti.',
           md: '# 在Ubuntu为特定用户安装全局NodeJS包\n\n在Ubuntu上通过NPM以全局方式安装NodeJS包时经常出现与路径访问权限的问题'
