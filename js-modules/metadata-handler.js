d8c6f7bce4fd4e70c335964de04e26fbc26beed4
daveying <daveying@163.com>
2018-06-10 23:37:11 +0800

    add unit-test for metadatahandler

--- js-modules/metadata-handler.js
+++ js-modules/metadata-handler.js
@@ -6,15 +6,21 @@ Format
 var MetadataHandler = {} || MetadataHandler;
 
 (function (MetadataHandler) {
-    MetadataHandler.extractMetadata = function (mdStr) {
+    MetadataHandler.extractMetadata = function (mdStr, seperator) {
         var lines = mdStr.split('\n');
-        console.log('lines', lines);
-        var regexp = /\[_metadata_:([^\u0000-\u007F]+)\]:-\s+['"]([^\u0000-\u007F]+)['"]/u
+        var regexp = /\[_metadata_:([\u0000-\uffef]+)\]:-\s+['"]([\u0000-\uffef]+)['"]/u
         var result = {};
+        var started = false;
         for (var i = 0, l = lines.length; i < l; i++) {
             var pair = regexp.exec(lines[i]);
-            if (pair && pair[0] && pair[1]) {
-                result[pair[0]] = pair[1];
+            if (pair && pair[1] && pair[2]) {
+                started = true;
+                if (seperator) {
+                    var arr = pair[2].split(seperator);
+                }
+                result[pair[1]] = arr && arr.length > 1 ? arr : pair[2];
+            } else if (started) {
+                break;
             }
         }
         return result;
--- test/metadata-handler-test.js
+++ test/metadata-handler-test.js
@@ -2,10 +2,42 @@ var assert = require('assert');
 var MetadataHandler = require('../js-modules/metadata-handler');
 
 (function () {
-    describe('Single line of metadata, `[_metadata_:author]:- "daveying"`:', function () {
-        it('should return {"author: daveying"}', function () {
+    describe('Single line of metadata:', () => {
+        it('should return {"author": "daveying"}', () => {
             var results = MetadataHandler.extractMetadata('[_metadata_:author]:- "daveying"');
             assert.equal(results.author, 'daveying');
+        });
+        it('should return chinese characters correctly', () => {
+            var results = MetadataHandler.extractMetadata(`[_metadata_:author]:- '汉字'`);
+            assert.equal(results.author, '汉字');
+        });
+        it('should return key with spaces correctly', () => {
+            var results = MetadataHandler.extractMetadata(`[_metadata_:author 0]:- '汉字'`);
+            assert.equal(results['author 0'], '汉字');
+        });
+        it('should return value with space correctly', () => {
+            var results = MetadataHandler.extractMetadata(`[_metadata_:author]:- '汉字 中国'`);
+            assert.equal(results.author, '汉字 中国');
+        });
+    });
+    describe('Multi-line of metadata:', () => {
+        it('should return {"author": "daveying", "tags": "web"}', () => {
+            var results = MetadataHandler.extractMetadata(`[_metadata_:author]:- 'daveying'\n[_metadata_:tags]:- '汉字 chinese'`);
+            assert.equal(results.author, 'daveying');
+            assert.equal(results.tags, '汉字 chinese');
+        });
+        it('should just extract the metadata at the top', () => {
+            var results = MetadataHandler.extractMetadata(`\n\n[_metadata_:author]:- 'daveying'\n#Tile\n[_metadata_:tags]:- '汉字 chinese'`);
+            assert.equal(results.author, 'daveying');
+            assert.equal(results.tags, undefined);
+        });
+    });
+    describe('Support metadata value be array', () => {
+        it('should return array value', () => {
+            var results = MetadataHandler.extractMetadata(`[_metadata_:author]:- 'daveying'\n[_metadata_:tags]:- '汉字|chinese'`, '|');
+            assert.equal(results.author, 'daveying');
+            assert.equal(results.tags[0], '汉字');
+            assert.equal(results.tags[1], 'chinese');
         });
     });
 })();
