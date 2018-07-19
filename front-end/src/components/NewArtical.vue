36aebf6ede22ad69be0b878d56ce6892c3d663a3
daveying <daveying@163.com>
2018-07-19 23:54:36 +0800

    add splitjs

--- front-end/src/components/NewArtical.vue
+++ front-end/src/components/NewArtical.vue
@@ -1,15 +1,33 @@
 <template>
   <v-container fluid fill-height>
-    <v-card>
-      <h1>Hello</h1>
-    </v-card>
+    <vue-split style="width:100%"
+      :elements="[
+        '#slotOne',
+        '#slotTwo'
+      ]"
+      direction="horizontal"
+      :min-size="100"
+      :gutter-size="10"
+      :snap-offset="50"
+      :sizes="[25, 75]"
+    >
+      <div id="slotOne">
+        <h2>foo</h2>
+      </div>
+      <div id="slotTwo">
+        <h2>bar</h2>
+      </div>
+    </vue-split>
   </v-container>
 </template>
 
 <script>
+import VueSplit from 'vue-splitjs'
+
 export default {
   created () {
     this.$store.dispatch('setFullWidth', true)
-  }
+  },
+  components: {VueSplit}
 }
 </script>
