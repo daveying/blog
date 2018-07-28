dc0d80aa65fdddbfbc63d559f2c2edfc858ca517
Dave <daveying@163.com>
2018-07-29 01:26:34 +0800

    add vue-monaco-editor

--- front-end/src/components/NewArtical.vue
+++ front-end/src/components/NewArtical.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-container fluid style="height:100%" px-0 py-0>
+  <v-container fluid fill-height px-0 py-0>
     <vue-split style="width:100%"
       :elements="[
         '#fileNav',
@@ -7,20 +7,20 @@
       ]"
       direction="horizontal"
       :min-size="100"
-      :gutter-size="7"
+      :gutter-size="5"
       :snap-offset="50"
       :sizes="[25, 75]"
     >
-      <div id="fileNav">
+      <div class="file-navigator" id="fileNav">
         <file-navigator></file-navigator>
       </div>
-      <div id="mainWin">
+      <div id="mainWin" class="editor">
         <v-container fluid px-0 py-0>
           <v-layout align-content-start>
-            <span class="editing-item selected"
+            <span :class="`editing-item ${i === 1 ? 'selected' : ''}`"
               v-for="(item,i) in 5"
               :key="i">
-              sdfaowejknaldkda.md
+              {{ `sdfaowejknaldkda${i}.md` }}
             </span>
           </v-layout>
         </v-container>
@@ -33,7 +33,15 @@
           :sizes="[50, 50]"
         >
           <div id="editor">
-            <h1>Editor</h1>
+            <MonacoEditor
+              height="100%"
+              language="typescript"
+              :code="code"
+              :editorOptions="options"
+              @mounted="onMounted"
+              @codeChange="onCodeChange"
+              >
+          </MonacoEditor>
           </div>
           <div id="preview">
             <h1>Preview</h1>
@@ -46,25 +54,61 @@
 
 <script>
 import VueSplit from 'vue-splitjs'
+import MonacoEditor from 'vue-monaco-editor'
 
 export default {
   created () {
     this.$store.dispatch('setFullWidth', true)
   },
-  components: {VueSplit}
+  data () {
+    return {
+      code: '// Type away! \n',
+      options: {
+        selectOnLineNumbers: false,
+        theme: 'vs'
+      }
+    }
+  },
+  methods: {
+    onMounted (editor) {
+      this.editor = editor
+    },
+    onCodeChange (editor) {
+      console.log(editor.getValue())
+    }
+  },
+  components: {VueSplit, MonacoEditor}
 }
 </script>
 
 <style>
 .gutter {
-  background-color: #fafafa !important;
+  background-color: #eaeaea !important;
 }
 /* .gutter:hover {
   background-color: #e5e5e5 !important;
 } */
 
+.selected.editing-item {
+  padding: 10px 10px;
+  background-color: #fafafa;
+  border: solid 1px #fafafa;
+}
+
 .editing-item {
-  padding: 5px 10px;
+  padding: 10px 10px;
+  background-color: #eaeaea;
+  border: solid 1px #eaeaea;
+}
+
+.file-navigator {
+  background-color: #f0f0f0;
+  border: solid 1px #eaeaea;
+}
+
+.editor {
+  background-color: #f0f0f0;
+  border: solid 1px #eaeaea;
 }
 
 </style>
